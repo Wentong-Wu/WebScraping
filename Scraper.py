6c3345e7b1da9b6322d132917b074b89389e9f10
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-07-05 23:37:31 +0100

    Find all the merch products

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,4 @@
+from argparse import Action
 from lib2to3.pgen2 import driver
 from turtle import delay
 import webbrowser
@@ -6,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 class Scraper:
@@ -30,17 +32,36 @@ class Scraper:
             print("Loading took too much time!")
         return self.driver
     
-    def scraping_one_data(self):
+    def get_merch_product(self):
         self.load_and_accept_cookies()
-        list_of_links = []
         time.sleep(2)
         #elements = self.driver.find_elements(By.XPATH,'//a[@class="product-link-box"]')
+        a = ActionChains(self.driver)
+        m = self.driver.find_element(By.XPATH,'//li[@id="merchandise"]')
+        a.move_to_element(m).perform()
+        n = self.driver.find_element(By.XPATH,'//li[@id="all-merchandise"]')
+        a.move_to_element(n).click().perform()
+
+        #Scroll all the way down to get all the products
+        length_of_page = self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);var length_of_page=document.body.scrollHeight;return length_of_page;")
+        page_end = False
+        while(page_end==False):
+            last_length_page = length_of_page
+            time.sleep(3)
+            length_of_page = self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight);var length_of_page=document.body.scrollHeight;return length_of_page;")
+            if last_length_page==length_of_page:
+                page_end=True
+
+        #Gets all the link to the products
+        list_of_links = []
+        time.sleep(2)
         elements = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH,'//a[@class="product-link-box"]')))
         for elem in elements:
             list_of_links.append(elem)
         print(list_of_links)
         time.sleep(3)
-        list_of_links[4].click()
+        print(list_of_links)
+
         # suselements = WebDriverWait(self.driver,self.delay).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@aria-hidden="false"]')))
         # print(suselements)
         pass
@@ -74,4 +95,4 @@ class Scraper:
                 page_end=True
 
 web = Scraper()
-web.scraping_one_data()
+web.get_merch_product()
