be089a6a7fc9b320ae51317340ab76a62dd51e2a
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-07-12 20:30:10 +0100

    Store raw data and images into folder

--- Scraper.py
+++ Scraper.py
@@ -3,12 +3,16 @@ from cgi import print_arguments
 from lib2to3.pgen2 import driver
 from math import prod
 from operator import truediv
-from os import link
+import os
 from re import M
 from turtle import delay
 from unicodedata import name
 import webbrowser
 import uuid
+import json
+import urllib.request
+from pathlib import Path
+from xml.dom.minidom import Element
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -108,10 +112,10 @@ class Scraper:
         self.driver.find_element(By.XPATH, "//button[@data-internal-id='save-birthday']").click()
         pass
 
-    def get_one_data(self, one_link,product_single_dict):
+    def get_one_data(self, one_link):
         #get pass age restriction if there is any
         self.driver.get(one_link)
-        
+        self.product_single_dict={}
         if(self.age_restriction_pass == False):
             try:
                 self.get_age_restriction()
@@ -126,36 +130,52 @@ class Scraper:
             self.driver.find_element(By.XPATH, "//a[@class='dropdown-item']").click()
             one_link = self.driver.current_url
         #get data from the page
-        product_title = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.CLASS_NAME, "product-title"))).get_attribute("textContent"))
+        self.product_title = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.CLASS_NAME, "product-title"))).get_attribute("textContent"))
         # try:
         #     product_price = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@data-internal-id='product-strike-through']"))).get_attribute("textContent"))
         # except:
         #product_price
-        product_price = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@data-internal-id='product-price']"))).get_attribute("textContent"))
+        self.product_price = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@data-internal-id='product-price']"))).get_attribute("textContent"))
         #product_status
-        product_status = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@id='buy_button']"))).get_attribute("textContent"))
+        self.product_status = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@id='buy_button']"))).text)
         #product_image
-        product_image = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@class='boxshot lazyloaded']"))).get_attribute("srcset"))
-        #product-SKU
-        product_SKU = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@class='product-info-details-table table-responsive']/table/tbody/tr[td[contains(.,'SKU')]]/td[2]"))).text)
+        self.product_image = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@class='boxshot lazyloaded']"))).get_attribute("srcset"))
+        self.product_image = self.product_image.replace(" ","").replace("1x","").replace("2x","").split(',')
         
-        print(product_SKU)
-        product_single_dict["title"] = product_title
-        product_single_dict["price"] = product_price
-        product_single_dict["status"] = product_status
-        product_single_dict["image"] = product_image
-        product_single_dict["SKU"] = product_SKU
-        product_single_dict["Link"] = one_link
-        product_single_dict["UUID"] = uuid.uuid4().hex
-        return product_single_dict
+        #product-SKU
+        self.product_SKU = (WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, "//*[@class='product-info-details-table table-responsive']/table/tbody/tr[td[contains(.,'SKU')]]/td[2]"))).text)
+        
+        #Save images into images folder
+        base = Path('images')
+        base.mkdir(exist_ok=True)
+        filename = os.path.join(base, ""+self.product_SKU+".jpg")
+        for imglink in self.product_image:
+            urllib.request.urlretrieve(imglink,filename)
 
-    def get_all_data(self,product_dict = [],product_single_dict = {}):
-        self.all_links = web.get_all_product_links()
+        self.product_single_dict["title"] = self.product_title
+        self.product_single_dict["price"] = self.product_price.replace("\n","")
+        self.product_single_dict["status"] = self.product_status
+        self.product_single_dict["image"] = self.product_image
+        self.product_single_dict["SKU"] = self.product_SKU
+        self.product_single_dict["Link"] = one_link
+        self.product_single_dict["UUID"] = uuid.uuid4().hex
+        return self.product_single_dict
+
+    def get_all_data(self):
+        #self.all_links = web.get_all_product_links()
+        self.product_dict = []
         #loop get_one_data with all the data
-        for link in self.all_links:
-            product_single_dict = self.get_one_data(link,product_single_dict)
-            product_dict.append(product_single_dict)
-        print(len(product_dict))
+        links = ["https://store.eu.square-enix-games.com/en_GB/product/725931/chrono-cross-the-radical-dreamers-edition-steam","https://store.eu.square-enix-games.com/en_GB/product/564495/final-fantasy-vii-remake-1st-class-edition-ps4"]
+        for link in links:
+            self.product_single_dict = self.get_one_data(link)
+            self.product_dict.append(self.product_single_dict)
+        base = Path('raw_data')
+        base.mkdir(exist_ok=True)
+        with open(base/'data.json','w',encoding='utf-8') as f:
+            json.dump(self.product_dict,f,ensure_ascii=False,indent=4)
+        
+        
+
 
 if __name__ == "__main__":
     web = Scraper()
