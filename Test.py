ad27150d6cb9b5cb3e6265bb82fa12a12fd2dbcb
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-07-18 20:27:46 +0100

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,24 @@
+import unittest
+from Scraper import Scraper
+
+class Test_Scraper(unittest.TestCase):
+
+    def test_accept_cookies(self):
+        pass
+
+    def test_get_product(self):
+        pass
+
+    def test_get_age_restriction(self):
+        pass
+
+    def test_get_one_data(self):
+        pass
+
+    def test_get_all_data(self):
+        pass
+
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
